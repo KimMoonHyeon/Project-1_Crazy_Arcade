6ad010b924908ae11d863f79f502d7dd14a8be7a
KimMoonHyeon <93696279+KimMoonHyeon@users.noreply.github.com>
2022-04-06 03:21:18 +0900

    물풍성 생선 및 사라짐, 캐릭터 스피드 증가

--- Assets/Scripts/Player_S.cs
+++ Assets/Scripts/Player_S.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class Player_S : MonoBehaviour
 {
     private Vector3 move_Vec;
+    public List<GameObject> Water_List = new List<GameObject>();
 
+    // 물풍선 배열을 만드는데, 크기는 일단 water_length만큼 고정으로 만들긴 해야하고, 
+
+    public GameObject Water;
+
+    public int water_max;
     public float speed;
 
     Rigidbody rigid;
 
     void Start()
     {
-        
-
+        water_max = 3;
         rigid = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         Plyaer_Move();
+        Plyaer_Skiil(); 
     }
-
 
     void Plyaer_Move()
     {
@@ -36,14 +41,51 @@ public class Player_S : MonoBehaviour
         if(m_z != 0 || m_x != 0)
             this.transform.rotation = Quaternion.LookRotation(new Vector3(m_x,0,m_z));
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //제자리에서 space 누를 경우에는 Trigger 해제해줘야지.
-            Debug.Log("물풍성 발사");
-
-        }
-
     }
 
+    void Plyaer_Skiil()
+    {
+        //space -> 물풍선 생성
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Debug.Log(Water_List.Count);
+            if (Water_List.Count < water_max)
+            {
+                // 1,2,3
+                GameObject new_Water = Instantiate(Water, new Vector3(transform.position.x, 0.3f, transform.position.z), Quaternion.identity);
+                Water_List.Add(new_Water);
+                new_Water.gameObject.GetComponent<Collider>().isTrigger = true;
+                StartCoroutine("Water_Boom");
+                //터질 때, 3d 큐브로 한 번 재현해보기 Trigger 이용해서 충돌판정
+            }
+            
+        }
+        else if (Input.GetKeyDown("1")) //스피드 증가
+        {
+            speed += 3f;
+        }
+        else if (Input.GetKeyDown("2")) //밀기
+        {
 
+        }
+        else if (Input.GetKeyDown("3")) //물풍선 개수 추가
+        {
+            water_max++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.isTrigger = false;
+    }
+    OnCollision();
+    
+
+    //코루틴 함수
+    IEnumerator Water_Boom()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(Water_List[0]);
+        Water_List.RemoveAt(0);
+    }
 }
